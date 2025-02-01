a130f658cc9f894806adae0709ff1faf0ff6d098
Utkal Behera <utkalbehera59@gmail.com>
2025-02-01 18:48:47 +0530

    Web cookies, explore how cookies works sending cookies, cookies parser, signed cookies

--- classroom/server.js
+++ classroom/server.js
@@ -2,8 +2,33 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser("secretcode"));
+
+app.get("/getsignedcvookie", (req, res) => {
+    res.cookie("madeIn", "India", { signed: true });
+    res.send("signed cookies sent");
+});
+
+app.get("/verify", (req, res) => {
+    console.log(req.signedCookies);
+    res.send("verified");
+});
+
+app.get("/getcookies", (req, res) => {
+    res.cookie("greet", "namaste");
+    res.cookie("madeIn", "India");
+    res.send("sent you some cookies!");
+});
+
+app.get("/greet", (req, res) => {
+    let { name = "anonymous" } = req.cookies;
+    res.send(`Hi, ${name}`);
+});
 
 app.get("/", (req, res) => {
+    console.dir(req.cookies);
     res.send("Hi I am root!");
 });
 
