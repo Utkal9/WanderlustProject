d83b1263024f00168916f28bc12c609690569fea
Utkal Behera <utkalbehera59@gmail.com>
2025-02-08 19:53:01 +0530

    Explore espress session, sessions options, storing and using session info

--- classroom/server.js
+++ classroom/server.js
@@ -2,38 +2,72 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
-const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
-app.use(cookieParser("secretcode"));
+const sessionOptions = {
+    secret: "mysupersecretstring",
+    resave: false,
+    saveUninitialized: true,
+};
+app.use(session(sessionOptions));
 
-app.get("/getsignedcvookie", (req, res) => {
-    res.cookie("madeIn", "India", { signed: true });
-    res.send("signed cookies sent");
+app.get("/register", (req, res) => {
+    let { name = "anonymous" } = req.query;
+    req.session.name = name;
+    req.redirect("/hello");
+    // console.log(req.session.name);
+    // res.send(name);
 });
 
-app.get("/verify", (req, res) => {
-    console.log(req.signedCookies);
-    res.send("verified");
+app.get("/hello", (req, res) => {
+    res.send(`hello, ${req.session.name}`);
 });
 
-app.get("/getcookies", (req, res) => {
-    res.cookie("greet", "namaste");
-    res.cookie("madeIn", "India");
-    res.send("sent you some cookies!");
-});
+// app.get("/reqcount", (req, res) => {
+//     if (req.session.count) {
+//         req.session.count++;
+//     } else {
+//         req.session.count = 1;
+//     }
+//     res.send(`You sent a request ${req.session.count} times`);
+// });
 
-app.get("/greet", (req, res) => {
-    let { name = "anonymous" } = req.cookies;
-    res.send(`Hi, ${name}`);
-});
+// app.get("/test", (req, res) => {
+//     res.send("test successfull!");
+// });
 
-app.get("/", (req, res) => {
-    console.dir(req.cookies);
-    res.send("Hi I am root!");
-});
+// const cookieParser = require("cookie-parser");
 
-app.use("/users", users);
-app.use("/posts", posts);
+// app.use(cookieParser("secretcode"));
+
+// app.get("/getsignedcvookie", (req, res) => {
+//     res.cookie("madeIn", "India", { signed: true });
+//     res.send("signed cookies sent");
+// });
+
+// app.get("/verify", (req, res) => {
+//     console.log(req.signedCookies);
+//     res.send("verified");
+// });
+
+// app.get("/getcookies", (req, res) => {
+//     res.cookie("greet", "namaste");
+//     res.cookie("madeIn", "India");
+//     res.send("sent you some cookies!");
+// });
+
+// app.get("/greet", (req, res) => {
+//     let { name = "anonymous" } = req.cookies;
+//     res.send(`Hi, ${name}`);
+// });
+
+// app.get("/", (req, res) => {
+//     console.dir(req.cookies);
+//     res.send("Hi I am root!");
+// });
+
+// app.use("/users", users);
+// app.use("/posts", posts);
 
 app.listen(3000, () => {
     console.log("Server is listing to 3000");
